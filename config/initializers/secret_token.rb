f918540c08a480c1e49e691bb2db804d7c8b9b3e
Yonghwan SO <sio4@users.sf.net>
2013-01-03 15:58:59 +0900

    new rails app 'siso'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siso::Application.config.secret_token = '54dd55553432f424648db9cef7859b07aa45a9ed3a50b3c6b0dd9fe9810675891be02ba275644ccbfe3eeb63ebb93bbdf6d01ad505db57ebaba1e1d223388e54'
